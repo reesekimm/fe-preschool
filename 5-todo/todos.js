2e972e4c392b546d51df7fba12904eaccd922b3b
Nanhyun Kim <nanhyunkim@gmail.com>
2019-12-23 16:12:28 +0900

    Complete Chapter 5 'Todos'

---
+++ 5-todo/todos.js
@@ -0,0 +1,54 @@
+const todos = require("./todo-data");
+
+const countTasks = () => {
+  let todo = 0,
+    doing = 0,
+    done = 0;
+  todos.forEach(data => {
+    switch (data.status) {
+      case "todo":
+        todo++;
+        break;
+      case "doing":
+        doing++;
+        break;
+      case "done":
+        done++;
+        break;
+      default:
+        return;
+    }
+  });
+  console.log(`현재상태: todo: ${todo}개, doing: ${doing}개, done: ${done}개`);
+};
+
+const filterTasks = status => {
+  const filtered = todos.reduce((result, curr) => {
+    if (curr.status === status) {
+      result.push(curr.name);
+    }
+    return result;
+  }, []);
+
+  console.log(
+    `${status}리스트: 총 ${filtered.length}건 - ${filtered.join(", ")}`
+  );
+};
+
+const show = cmd => {
+  switch (cmd) {
+    case "all":
+      countTasks();
+      break;
+    case "todo":
+    case "doing":
+    case "done":
+      filterTasks(cmd);
+      break;
+    default:
+      console.log("유효한 명령어가 아닙니다.");
+  }
+};
+
+show("all");
+show("todo");
